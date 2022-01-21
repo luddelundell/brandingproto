997ea0ecc45a570c3fa1641dbfb898599ce501ce
Erik Lundell <erik.lundell@sitevision.se>
2022-01-21 10:12:50 +0100

    New text options

--- js/list.js
+++ js/list.js
@@ -17,5 +17,15 @@ $(document).ready(function(){
     $('.menuitem__button').click(function(){
         $(this).parent().find('ul').slideToggle();
         $(this).parent().toggleClass('expanded');
-    })
+    });
+    // function setBrand(){
+    //     var brand10 = $(':root').css('--brand')+'10';
+    //     var brand20 = $(':root').css('--brand')+'20';
+    //     var brand40 = $(':root').css('--brand')+'35';
+    //     $(':root').css('--brand10', brand10 );
+    //     $(':root').css('--brand20', brand20 );
+    //     $(':root').css('--brand40', brand40 );
+    // };
+      
+    // setBrand();
 });
